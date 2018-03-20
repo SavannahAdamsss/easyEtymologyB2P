d65f74cb3a43cf3bbb3ccfcb4c472fa1b6d04664
SavannahAdamsss <savannah.adams0@gmail.com>
2018-03-20 15:49:27 -0600

    changes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # easyEtymologyB2P
-A basic website where one can search any English word and return the etymology of that word by calling the data from the Wordnik API.
+A basic website where one can search any English word and return the etymology of that word by calling the data from the an API by CJ.R.
--- form.js
+++ form.js
@@ -6,7 +6,6 @@ form.addEventListener('submit', event => {
     const formData = new FormData(form);
         let input = document.getElementById('input');
             input.value = '';
-            console.log(input);
 
     fetch(`https://ety-api.now.sh/word/${formData.get('wordInput')}`)
         .then(function(response){
@@ -14,73 +13,20 @@ form.addEventListener('submit', event => {
         return response.json();
         })
         .then(function(response){
-            // console.log(response)
+            console.log(response);
     
-        const resultDiv = document.getElementsByClassName('ety')[0];
-            const title = textContent word 
-            title
+        const resultWord = document.getElementsByClassName('ety')[0];
+            let word = document.createElement('p');
+            word.textContent = response.word;
+            resultWord.appendChild(word);
 
+        const resultEty = document.getElementsByClassName('ety')[0];
+            let ety = document.createElement('p');
+            ety.textContent = response.ety;
+            resultEty.appendChild(ety);
 
-
-            
+            $(document).ready(function(){
+                $('.scrollspy').scrollSpy();
+              });
         });
 });
-
-
-
-// const url = 'http://api.wordnik.com:80/v4/word.json/word/etymologies?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
-
-// fetch(url);
-//     .then(response => response.json())
-//     .then(json => {
-        
-//     });
-
-// console.log(json);
-
-// function parseJson(response) {
-//     return response.json();
-// }
-
-// function logResult(result) {
-//     const children = result.data.children;
-// }
-
-// children.forEach(child) => {
-//     const li = document.createElement('li');
-//     li.textContent = child.data.title;
-//     list.appendChild(li);
-// }
-
-// function load() {
-//     fetch("http://api.wordnik.com:80/v4/word.json/word/etymologies?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5");
-//         .then(response => console.log(response.json));
-// }
-
-// const data = {
-//     message
-// };
-
-// sendMessage(data);
-// }
-
-// function sendMessage(data) {
-//     const url = 'http://api.wordnik.com:80/v4/word.json/word/etymologies?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
-//     const body = JSON.stringify(data);
-//     fetch(url, {
-//         method: 'POST',
-//         body,
-//         headers: {
-//             'content-type': 'application/json'
-//         }
-//     }).then(parseJson)
-//         .then(showResponse);
-// }
-
-// function parseJson(response) {
-//     return response.json();
-// }
-
-// function showResponse(response) {
-//     return response.json();
-// }
