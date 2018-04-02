9c4fa2141e7d7f303bf15f294fd1d113e4fc35c8
SavannahAdamsss <savannah.adams0@gmail.com>
2018-04-02 12:41:06 -0600

    final

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # easyEtymologyB2P
 A basic website where one can search any English word and return the etymology of that word by calling the data from the an API by CJ.R.
+
+By clicking the sound icon, one can hear the pronunciation of the generated word, which is from a second API by Jasen Hill:
+https://word-to-mp3-api.herokuapp.com/
--- form.js
+++ form.js
@@ -1,71 +1,67 @@
+var inputWord = '';
 const form = document.querySelector('form');
 
 form.addEventListener('submit', event => {
     event.preventDefault();
+    let resultWord = document.getElementsByClassName('ety')[0];
+    resultWord.innerHTML = '';
 
     const formData = new FormData(form);
         let input = document.getElementById('input');
             input.value = '';
+    console.log(input.value);
 
     fetch(`https://ety-api.now.sh/word/${formData.get('wordInput')}`)
         .then(function(response){
             
         return response.json();
-        })
-        .then(function(response){
-            console.log(response);
+        }).then(function(response){
+            buildEty(response);
+        }).catch(function(response) {
+            console.log("fucked");
+        });
 
-        let soundIcon = `
-        <audio auto id="sound">
-        <source id="audio" src="" type="audio/mpeg">
-                Your browser does not support the audio element.
-        </audio>
-        <a onclick="document.getElementById('sound').play()">
-          <img src="speaking-ico.png">
-        </a>
-        `;
-    
-        let resultWord = document.getElementsByClassName('ety')[0];
-            resultWord.innerHTML = null;
+
+        function buildEty(response) {
+            let resultWord = document.getElementsByClassName('ety')[0];
             let word = document.createElement('p');
-            word.innerHTML = response.word + soundIcon;
+            word.innerHTML = response.word;
             resultWord.appendChild(word);
 
-        const resultEty = document.getElementsByClassName('ety')[0];
+            const resultEty = document.getElementsByClassName('ety')[0];
             let ety = document.createElement('p');
             ety.textContent = response.ety;
             resultEty.appendChild(ety);
+        }
 
-            // dom manipulate url of sound
-            let mp3 = "http://jmh-sounds.surge.sh/pussy_gb_1.mp3";
-            document.getElementById("audio").src = mp3;
-            
-        });
+
+        let url = `https://word-to-mp3-api.herokuapp.com/${formData.get('wordInput')}`;
+        fetch(url)
+            .then(function(response) {
+                    
+                return response.json();
+                })
+                .then(function(data) {
+                    console.log(data.mp3Url);
+                    getAudio(data);
+                });
+
 });
 
-// const url = 'https://api.oxforddictionaries.com/api/v1/entries/en/'
-// let searchString = 'word'
+function getAudio(data) {
+    let soundLink = data.mp3Url;
+    let soundHolder = document.querySelector('.sound-holder');
+    let soundItem =         
+    `<audio auto id="sound">
+     <source id="audio" src="${soundLink}" type="audio/mpeg">
+            Your browser does not support the audio element.
+    </audio>
+    <a onclick="document.getElementById('sound').play()">
+      <img src="speaking-ico.png">
+    </a>
+    `;
+    console.log(soundItem, "soundItem with link???");
+    soundHolder.innerHTML = soundItem;
 
-// var settings = {
-//   "async": true,
-//   "crossDomain": true,
-//   "method": "GET",
-//   'mode': 'no-cors',
-//   "headers": {
-//     "Host": 'od-api.oxforddictionaries.com',
-//     "Accept": 'application/json',
-//     "app_id": 'fb405eb4',
-//     "app_key": 'bad3a11b2954277c3a08ddbbaaec4216',
-//   }
-// }
+}
 
-// fetch(url + searchString, settings)
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(data) {
-//       return data.results.lexicalEntries.pronunciations.audioFile
-//     });
-
-
-// to add IPA word after little man, write an if statement saying if 'key' = true something something something
