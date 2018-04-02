f24d0f4244ac83538725138c8a53594f5e9075da
SavannahAdamsss <savannah.adams0@gmail.com>
2018-04-02 15:51:36 -0600

    better

--- form.js
+++ form.js
@@ -1,4 +1,3 @@
-var inputWord = '';
 const form = document.querySelector('form');
 
 form.addEventListener('submit', event => {
@@ -6,21 +5,40 @@ form.addEventListener('submit', event => {
     let resultWord = document.getElementsByClassName('ety')[0];
     resultWord.innerHTML = '';
 
-    const formData = new FormData(form);
-        let input = document.getElementById('input');
-            input.value = '';
-    console.log(input.value);
+    let input = document.getElementById('input');
+    let userInput = input.value;
+    console.log(userInput, "word day entered");
+    
+    fetchWordData();
+    fetchAudioData();
 
-    fetch(`https://ety-api.now.sh/word/${formData.get('wordInput')}`)
-        .then(function(response){
-            
-        return response.json();
-        }).then(function(response){
-            buildEty(response);
-        }).catch(function(response) {
-            console.log("fucked");
-        });
 
+    function fetchWordData() {
+        let input = document.querySelector("#input");
+        let userInput = input.value;
+        let url = "https://ety-api.now.sh/word/" + userInput;
+        fetch(url)
+            .then(function(response){  
+                return response.json();
+            }).then(function(response){
+                buildEty(response);
+            }).catch(function(response) {
+                console.log("fucked");
+            });
+    }
+
+    function fetchAudioData() {
+        let input = document.querySelector("#input");
+        let userInput = input.value;
+        let url = "https://word-to-mp3-api.herokuapp.com/" + userInput;
+        fetch(url)
+            .then(function(response) {
+                return response.json();
+            }).then(function(data) {
+                console.log(data.mp3Url);
+                getAudio(data);
+            });
+    }
 
         function buildEty(response) {
             let resultWord = document.getElementsByClassName('ety')[0];
@@ -34,34 +52,22 @@ form.addEventListener('submit', event => {
             resultEty.appendChild(ety);
         }
 
-
-        let url = `https://word-to-mp3-api.herokuapp.com/${formData.get('wordInput')}`;
-        fetch(url)
-            .then(function(response) {
-                    
-                return response.json();
-                })
-                .then(function(data) {
-                    console.log(data.mp3Url);
-                    getAudio(data);
-                });
+        function getAudio(data) {
+            let soundLink = data.mp3Url;
+            let soundHolder = document.querySelector('.sound-holder');
+            let soundItem =         
+            `<audio auto id="sound">
+             <source id="audio" src="${soundLink}" type="audio/mpeg">
+                    Your browser does not support the audio element.
+            </audio>
+            <a onclick="document.getElementById('sound').play()">
+              <img src="speaking-ico.png">
+            </a>
+            `;
+            console.log(soundItem, "soundItem with link???");
+            soundHolder.innerHTML = soundItem;
+        
+        }
 
 });
-
-function getAudio(data) {
-    let soundLink = data.mp3Url;
-    let soundHolder = document.querySelector('.sound-holder');
-    let soundItem =         
-    `<audio auto id="sound">
-     <source id="audio" src="${soundLink}" type="audio/mpeg">
-            Your browser does not support the audio element.
-    </audio>
-    <a onclick="document.getElementById('sound').play()">
-      <img src="speaking-ico.png">
-    </a>
-    `;
-    console.log(soundItem, "soundItem with link???");
-    soundHolder.innerHTML = soundItem;
-
-}
 
